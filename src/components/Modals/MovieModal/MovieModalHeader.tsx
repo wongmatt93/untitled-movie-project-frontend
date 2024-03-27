4839b6b4551aca6ce571135ce6d5ceffe74f4e04
Matthew <wongmatt93@gmail.com>
2024-03-27 13:32:21 -0400

    updated ui for home page and modals

--- src/components/Modals/MovieModal/MovieModalHeader.tsx
+++ src/components/Modals/MovieModal/MovieModalHeader.tsx
@@ -1,4 +1,10 @@
-import { IonButton, IonHeader, IonIcon, IonToolbar } from "@ionic/react";
+import {
+  IonButton,
+  IonButtons,
+  IonHeader,
+  IonIcon,
+  IonToolbar,
+} from "@ionic/react";
 import { closeOutline } from "ionicons/icons";
 import Movie from "../../../models/Movie";
 import UserProfile from "../../../models/UserProfile";
@@ -21,39 +27,43 @@ const MovieModalHeader = ({ movie, userProfile }: Props) => {
 
   return (
     <IonHeader className="MovieModalHeader">
-      <IonToolbar>
-        <IonButton
-          className="movie-modal-close-button"
-          size="small"
-          color="light"
-          shape="round"
-          onClick={() => setIsOpen(false)}
-        >
-          <IonIcon slot="icon-only" icon={closeOutline} />
-        </IonButton>
+      <IonToolbar className="movie-modal-toolbar">
+        <IonButtons slot="end">
+          <IonButton
+            className="movie-modal-close-button"
+            fill="solid"
+            shape="round"
+            color="light"
+            onClick={() => setIsOpen(false)}
+          >
+            <IonIcon slot="icon-only" icon={closeOutline} />
+          </IonButton>
+        </IonButtons>
+      </IonToolbar>
+      <section className="movie-modal-backdrop-info-container">
         <img
           className="movie-modal-header-backdrop"
           src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`}
           alt={`${title} backdrop`}
         />
-      </IonToolbar>
-      <div className="movie-modal-title-button-container">
-        <h2 className="movie-modal-title">{title}</h2>
-        {userProfile && (
-          <div>
-            <AddRemoveMovieButton
-              type="watched"
-              userProfile={userProfile}
-              movieId={id}
-            />
-            <AddRemoveMovieButton
-              type="watchlist"
-              userProfile={userProfile}
-              movieId={id}
-            />
-          </div>
-        )}
-      </div>
+        <div className="movie-modal-title-button-container">
+          <h2 className="movie-modal-title">{title}</h2>
+          {userProfile && (
+            <div>
+              <AddRemoveMovieButton
+                type="watched"
+                userProfile={userProfile}
+                movieId={id}
+              />
+              <AddRemoveMovieButton
+                type="watchlist"
+                userProfile={userProfile}
+                movieId={id}
+              />
+            </div>
+          )}
+        </div>
+      </section>
     </IonHeader>
   );
 };
